447f48cb9c2e20dd5b562bf3e8a03d47d1d7e5d3
Shashikant <Shashikant>
2018-04-29 19:46:13 +0530

    Initial commit

---
+++ src/test/java/com/letskodeit/constants/Auth.java
@@ -0,0 +1,8 @@
+package com.letskodeit.constants;
+
+public class Auth {
+	public static final String CONSUMER_KEY = "Xaf3qCeBDKTg8Q57jqub2bdpQ";
+	public static final String CONSUMER_SECRET = "i68HcDppio8ITDO4at6xFPDGNIDuCJ1Ula7DARkzhFKobXpVdv";
+	public static final String ACCESS_TOKEN = "990527525347577856-UNhrgWdoawSmuIUdpqQ6NxDo7fBSglq";
+	public static final String ACCESS_SECRET = "6A84vx4Fe7Sak0LPrpkXJVfKpukBIkZSkz3ZYj1ztBUgD";
+}
